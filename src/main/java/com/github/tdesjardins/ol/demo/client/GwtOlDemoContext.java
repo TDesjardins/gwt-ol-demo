2ff51e8a8d1841d8c6642a927e5bf54943f7733f
Tino Desjardins <tino.desjardins@arcor.de>
2019-08-29 09:59:09 +0200

    Make Nalu 2.0.0 compatible

--- src/main/java/com/github/tdesjardins/ol/demo/client/GwtOlDemoContext.java
+++ src/main/java/com/github/tdesjardins/ol/demo/client/GwtOlDemoContext.java
@@ -1,11 +1,18 @@
 package com.github.tdesjardins.ol.demo.client;
 
-import com.github.nalukit.nalu.client.application.IsContext;
+import com.github.nalukit.nalu.client.context.Context;
+import com.github.nalukit.nalu.client.context.IsContext;
 
 public class GwtOlDemoContext implements IsContext {
 
     public GwtOlDemoContext() {
         // enter your constructor code here ...
+    }
+
+    @Override
+    public Context getContext() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
