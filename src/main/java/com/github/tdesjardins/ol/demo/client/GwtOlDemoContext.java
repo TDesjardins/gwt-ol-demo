e187b6f1e9755ae2f580bce4ee383ee18ba3fcca
Tino Desjardins <tino.desjardins@arcor.de>
2019-08-31 07:27:22 +0200

    Remove getContext from interface IsContext

--- src/main/java/com/github/tdesjardins/ol/demo/client/GwtOlDemoContext.java
+++ src/main/java/com/github/tdesjardins/ol/demo/client/GwtOlDemoContext.java
@@ -1,18 +1,11 @@
 package com.github.tdesjardins.ol.demo.client;
 
-import com.github.nalukit.nalu.client.context.Context;
 import com.github.nalukit.nalu.client.context.IsContext;
 
 public class GwtOlDemoContext implements IsContext {
 
     public GwtOlDemoContext() {
         // enter your constructor code here ...
-    }
-
-    @Override
-    public Context getContext() {
-        // TODO Auto-generated method stub
-        return null;
     }
 
 }
